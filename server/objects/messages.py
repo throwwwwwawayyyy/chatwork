1a4f8d3e43ff53bddba04744cb7c1348896c1709
throwwwwwawayyyy <fsdijfnasinfdsfdsgfdgsdfgdsf@gmail.com>
2024-07-18 11:34:46 +0300

    Migrated server to JSON

--- server/objects/messages.py
+++ server/objects/messages.py
@@ -1,10 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import json
 import utils.constants as constants
 
 
 class Message:
-    def serialize(self):
-        print("Don't instantiate a Message type directly. Use a subclass")
+    def __init__(self):
+        print("Don't instantiate this class directly, use a subclass")
+
+    def serialize(self) -> bytes:
+        return bytes(json.dumps(asdict(self)))
 
 
 @dataclass
@@ -12,34 +16,18 @@ class ClientMessage(Message):
     username: str
     content: bytes
 
-    def serialize(self) -> bytes:
-        result = f"{constants.MessageType.CLIENT.value}{constants.SEP}{self.username}{constants.SEP}{self.content.decode('utf-8')}"
-        return result.encode('utf-8')
-
 
 @dataclass
 class AckMessage(Message):
     code: constants.AckCodes
 
-    def serialize(self) -> bytes:
-        result = f"{constants.MessageType.ACK.value}{constants.SEP}{self.code.value}"
-        return result.encode('utf-8')
-
 
 @dataclass
 class JoinMessage(Message):
     username: str
-    privilage: int
-
-    def serialize(self) -> bytes:
-        result = f"{constants.MessageType.JOIN.value}{constants.SEP}{self.username}{constants.SEP}{self.privilage}"
-        return result.encode('utf-8')
+    privilege: int
 
 
 @dataclass
 class LeaveMessage(Message):
     username: str
-
-    def serialize(self) -> bytes:
-        result = f"{constants.MessageType.LEAVE.value}{constants.SEP}{self.username}"
-        return result.encode('utf-8')
